5886bb97829cff180f1257c6f983acfc27192d17
Ravi Prakash Singh <ravisingh1651@gmail.com>
2023-08-15 00:21:17 +0530

    Added changes to Readme and tracker file

--- README.md
+++ README.md
@@ -1,1 +1,95 @@
 # Mini-Torrent
+The Mini-torrent project is basically a peer-to-peer file sharing network. The user has functionalities like sharing a file, downloading a file, removing a file from sharing etc. It is basically like the Bit-Torrent we have on the internet.
+
+The architecture is such that there are multiple clients(users) and a tracker which stores the metadata of which all users have a file, basically a file-user mapping.
+
+image
+
+Prerequisites
+Software Requirement
+
+G++ compiler
+
+To install G++ : sudo apt-get install g++
+OpenSSL library
+
+To install OpenSSL library : sudo apt-get install openssl
+Platform: Linux
+
+Installation
+1. cd client
+2. make
+3. cd ../tracker
+5. make
+6. cd ..
+Usage
+Tracker
+Run Tracker:
+cd tracker
+./tracker​ <TRACKER INFO FILE> <TRACKER NUMBER>
+ex: ./tracker tracker_info.txt 1
+<TRACKER INFO FILE> contains the IP, Port details of all the trackers.
+
+Ex:
+127.0.0.1
+5000
+127.0.0.1
+6000
+Close Tracker:
+quit
+Client:
+Run Client:
+cd client
+./client​ <IP>:<PORT> <TRACKER INFO FILE>
+ex: ./client 127.0.0.1:18000 tracker_info.txt
+Create user account:
+create_user​ <user_id> <password>
+Login:
+login​ <user_id> <password>
+Create Group:
+create_group​ <group_id>
+Join Group:
+join_group​ <group_id>
+Leave Group:
+leave_group​ <group_id>
+List pending requests:
+list_requests ​<group_id>
+Accept Group Joining Request:
+accept_request​ <group_id> <user_id>
+List All Group In Network:
+list_groups
+List All sharable Files In Group:
+list_files​ <group_id>
+Upload File:
+​upload_file​ <file_path> <group_id​>
+Download File:​
+download_file​ <group_id> <file_name> <destination_path>
+Logout:​
+logout
+Show_downloads: ​
+show_downloads
+Stop sharing: ​
+stop_share ​<group_id> <file_name>
+Working
+User should create an account and register with tracker.
+Login using the user credentials.
+Tracker maintains information of clients with their files(shared by client) to assist the clients for the communication between peers.
+User can create Group and hence will become admin of that group.
+User can fetch list of all Groups in server.
+User can join/leave group.
+Group admin can accept group join requests.
+Share file across group: Shares the filename and SHA1 hash of the complete file as well as piecewise SHA1 with the tracker.
+Fetch list of all sharable files in a Group.
+Download:
+Retrieve peer information from tracker for the file.
+Download file from multiple peers (different pieces of file from different peers - ​piece selection algorithm​) simultaneously and all the files which client downloads will be shareable to other users in the same group. File integrity is ensured using SHA1 comparison.
+Piece selection algorithm used: Selects random piece and then downloads it from a random peer having that piece.
+Show downloads.
+Stop sharing file.
+Logout - stops sharing all files.
+Whenever client logins, all previously shared files before logout should automatically be on sharing mode.
+Assumptions
+Only one tracker is implemented and that tracker should always be online.
+The peer can login from different IP addresses, but the details of his downloads/uploads will not be persistent across sessions.
+SHA1 integrity checking doesn't work correctly for binary files, even though in most likelihood the file would have downloaded correctly.
+File paths should be absolute.
--- tracker.cpp
+++ tracker.cpp
@@ -41,3 +41,516 @@ void error(const char *msg)
     perror(msg);
     exit(1);
 }
+string extractcmd(string buffer)
+{
+    vector<string> tokens;
+    stringstream ss(buffer);
+    while (ss.good())
+    {
+        string sb;
+        getline(ss, sb, ' ');
+        tokens.push_back(sb);
+    }
+    string command = tokens[0];
+
+    if (command == "create_user")
+    {
+        if (user_list.find(tokens[1]) == user_list.end())
+        {
+            struct users peer;
+            string uid = tokens[1];
+            string pass = tokens[2];
+            string ip_port = tokens[3];
+            peer.UID = uid;
+            peer.PASS = pass;
+            peer.IP_PORT = ip_port;
+            user_list[uid] = peer;
+            return "user created successfully";
+        }
+        else
+            return "user already exist";
+    }
+
+    else if (command == "login")
+    {
+        // cout << tokens[2] << " " << user_list[tokens[1]].PASS << endl;
+
+        if (user_list.find(tokens[1]) == user_list.end())
+            return "User not registered";
+        else
+        {
+            if (user_list[tokens[1]].LoginStatus == true)
+                return "Already logged in";
+            else if (user_list[tokens[1]].PASS == tokens[2])
+            {
+                user_list[tokens[1]].LoginStatus = true;
+                return "Login Successful";
+            }
+            else
+                return "Wrong Password";
+        }
+    }
+
+    else if (command == "logout")
+    {
+        // cout << "Inside logout" << endl;
+        if (user_list.find(tokens[1]) == user_list.end())
+            return "User not registered";
+        else
+        {
+            if (user_list[tokens[1]].LoginStatus == false)
+                return "Already logged out";
+            else
+                user_list[tokens[1]].LoginStatus = false;
+            return "Logout Successful";
+        }
+    }
+
+    else if (command == "create_group")
+    {
+
+        string gid = tokens[1], uid = tokens[2];
+        if (user_list[uid].LoginStatus == false)
+        {
+            return "Login First!";
+        }
+        if (group_list.find(gid) != group_list.end())
+        {
+            return "Group already exist.";
+        }
+        else
+        {
+            struct groups gp;
+            gp.UID = uid;
+            gp.GID = gid;
+            gp.members.insert(uid);
+            group_list[gid] = gp;
+            // admin[gid] = uid;
+            return "Group created sucessfully.";
+        }
+    }
+    else if (command == "join_group")
+    {
+        string gid = tokens[1], uid = tokens[2];
+        if (user_list[uid].LoginStatus == false)
+        {
+            return "Login First!";
+        }
+        else if (group_list.find(gid) != group_list.end())
+        {
+            if (group_list[gid].UID == tokens[2])
+            {
+                return ("You're the Admin of this group.");
+            }
+            else if (group_list[gid].members.find(uid) != group_list[gid].members.end())
+                return ("You're already a member of this group.");
+            else if (group_list[gid].pending_req.find(uid) != group_list[gid].pending_req.end())
+                return ("Your joining request is pending.");
+            group_list[gid].pending_req.insert(uid);
+            return ("Group joining request Sent.");
+        }
+        else
+        {
+            return ("Group Doesn't exist.");
+        }
+    }
+    else if (command == "leave_group")
+    {
+        string gid = tokens[1], uid = tokens[2];
+        if (user_list[uid].LoginStatus == false)
+        {
+            return "Login First!";
+        }
+        else if (group_list.find(gid) != group_list.end())
+        {
+            if (group_list[tokens[1]].members.find(uid) != group_list[gid].members.end())
+            {
+                group_list[gid].members.erase(uid);
+                return ("You've left the group.");
+            }
+            else
+                return ("You're not present in this group.");
+        }
+        else
+        {
+            return ("Group Doesn't exist.");
+        }
+    }
+
+    else if (command == "requests")
+    {
+        string gid = tokens[2], uid = tokens[3];
+        if (user_list[uid].LoginStatus == false)
+        {
+            return "Login First!";
+        }
+        else if (group_list.find(gid) != group_list.end())
+        {
+            if (group_list[gid].UID == uid)
+            {
+                if (group_list[gid].pending_req.empty())
+                    return ("No pending requests.");
+                else
+                {
+                    string s = "";
+                    for (auto id = group_list[gid].pending_req.begin(); id != group_list[gid].pending_req.end(); ++id)
+                    {
+                        s += "(";
+                        s += *id;
+                        s += ") ";
+                    }
+                    return s;
+                }
+            }
+            else
+                return ("You are not Admin of this group.");
+        }
+        else
+        {
+            return ("Group Doesn't exist.");
+        }
+    }
+
+    else if (command == "accept_request")
+    {
+        string gid = tokens[1], uid = tokens[3], peerid = tokens[2];
+        if (user_list[uid].LoginStatus == false)
+        {
+            return "Login First!";
+        }
+        else if (group_list.find(gid) != group_list.end())
+        {
+            if (group_list[gid].UID == uid)
+            {
+                if (group_list[gid].pending_req.find(peerid) == group_list[gid].pending_req.end())
+                    return ("No pending requests.");
+                else
+                {
+                    group_list[gid].members.insert(peerid);
+                    group_list[gid].pending_req.erase(peerid);
+                    string s = "Joining request accepted for " + peerid;
+
+                    return s;
+                }
+            }
+            else
+                return ("You are not Admin of this group.");
+        }
+        else
+        {
+            return ("Group Doesn't exist.");
+        }
+    }
+
+    else if (command == "list_groups")
+    {
+        string uid = tokens[1];
+        if (user_list[uid].LoginStatus == false)
+        {
+            return "Login First!";
+        }
+        if (group_list.empty())
+            return ("No Group exist.");
+        else
+        {
+            string s = "";
+            for (auto id : group_list)
+            {
+                s += "(";
+                s += id.first;
+                s += ") ";
+            }
+            return s;
+        }
+    }
+
+    else if (command == "upload_file")
+    {
+        string gid = tokens[2], uid = tokens[3], ip_port = tokens[4], path = tokens[1];
+        if (user_list[uid].LoginStatus == false)
+        {
+            return "Login First!";
+        }
+        else if (group_list.find(gid) != group_list.end())
+        {
+            if (group_list[gid].members.find(uid) == group_list[gid].members.end())
+            {
+                return ("You're not a member of this group.");
+            }
+            else
+            {
+                //extracting filename
+                int i = path.length() - 1;
+                while (i >= 0)
+                {
+                    if (path[i] == '/')
+                        break;
+                    i--;
+                }
+                string fileName;
+                if (i < path.length() - 1)
+                {
+                    fileName = path.substr(i + 1, path.length());
+                }
+                else
+                    return "Please provide absolute path.";
+
+                //file size
+                ifstream f(path, ios::ate | ios::binary);
+                long long fsize = f.tellg();
+                f.close();
+                struct files newfile;
+                newfile.GID = gid;
+                newfile.file_size = fsize;
+                newfile.file_name = fileName;
+                newfile.file_path = path;
+                newfile.seeders.insert({uid, ip_port});
+                if (file_list.find(fileName) != file_list.end())
+                {
+                    if (file_list[fileName].seeders.find({uid, ip_port}) != file_list[fileName].seeders.end())
+                    {
+                        return "U r already a seeder";
+                    }
+                    else
+                    {
+                        file_list[fileName].seeders.insert({uid, ip_port});
+                        return "One more seeder added for file " + fileName + " in " + gid + ".";
+                    }
+                }
+                else
+                {
+                    group_list[gid].file_names.insert(fileName);
+                    // file_list[fileName].seeders.insert({uid, ip_port});
+                    file_list[fileName] = newfile;
+                    return "New file uploaded successfully in " + gid + ".";
+                }
+            }
+        }
+        else
+        {
+            return ("Group Doesn't exist.");
+        }
+    }
+
+    else if (command == "list_files")
+    {
+        string gid = tokens[1], uid = tokens[2];
+        if (user_list[uid].LoginStatus == false)
+        {
+            return "Login First!";
+        }
+        else if (group_list.find(gid) != group_list.end())
+        {
+            if (group_list[tokens[1]].members.find(uid) != group_list[gid].members.end())
+            {
+                string s = "";
+                if (group_list[gid].file_names.empty())
+                    return "List is empty";
+                for (auto id : group_list[gid].file_names)
+                {
+                    s += "(";
+                    s += id;
+                    s += ") ";
+                }
+                return s;
+            }
+            else
+                return ("You're not present in this group.");
+        }
+        else
+        {
+            return ("Group Doesn't exist.");
+        }
+    }
+
+    else if (command == "download_file")
+    {
+        string gid = tokens[1], uid = tokens[4], filename = tokens[2];
+        if (user_list[uid].LoginStatus == false)
+        {
+            return "Login First!";
+        }
+        else if (group_list.find(gid) != group_list.end())
+        {
+            if (group_list[gid].members.find(uid) == group_list[gid].members.end())
+            {
+                return ("You're not a member of this group.");
+            }
+            else
+            {
+                if (file_list.find(filename) == file_list.end())
+                    return "No such file exist!";
+                else
+                {
+                    bool inside = false;
+                    string s = to_string(file_list[filename].seeders.size()) + " " + to_string(file_list[filename].file_size) + " " + file_list[filename].file_path;
+                    if(file_list[filename].seeders.empty())
+                    {
+                        return "No seeder available";
+                    }
+                    for (auto seeder : file_list[filename].seeders)
+                    {
+                        if (user_list[seeder.first].LoginStatus == false)
+                            continue;
+                        inside = true;
+                        s += " ";
+                        s += seeder.second;
+                    }
+                    if (inside == false)
+                        return ("Currently all seeders are offline.");
+                    return s;
+                }
+            }
+        }
+        else
+        {
+            return ("Group Doesn't exist.");
+        }
+    }
+    else if (command == "stop_share")
+    {
+        string gid = tokens[1], uid = tokens[3], fileName = tokens[2],ip_port = tokens[4];
+        if (user_list[uid].LoginStatus == false)
+        {
+            return "Login First!";
+        }
+        else if (group_list.find(gid) != group_list.end())
+        {
+            if (group_list[gid].members.find(uid) == group_list[gid].members.end())
+            {
+                return ("You're not a member of this group.");
+            }
+            else
+            {
+                if (file_list.find(fileName) != file_list.end())
+                {
+                    if (file_list[fileName].seeders.find({uid, ip_port}) == file_list[fileName].seeders.end())
+                    {
+                        return "U r not a seeder";
+                    }
+                    else
+                    {
+                        file_list[fileName].seeders.erase({uid, ip_port});
+                        return uid+" stopped sharing";
+                    }
+                }
+                else
+                {
+                    return "File doesn't exist";
+                }
+            }
+        }
+        else
+        {
+            return ("Group Doesn't exist.");
+        }
+    }
+    else
+    {
+        return ("Command invalid");
+    }
+}
+
+void *reader(void *param)
+{
+    int *newsock = (int *)(param);
+    int newsockfd = *newsock;
+    char buffer[256];
+    int n;
+    while (1)
+    {
+        if (newsockfd < 0)
+            error("ERROR on accept");
+        bzero(buffer, 256);
+
+        if (n = read(newsockfd, buffer, 255) <= 0)
+        {
+            close(newsockfd);
+            return NULL;
+        }
+        string outputcmd = extractcmd(buffer);
+        char buff[256];
+        strcpy(buff, outputcmd.c_str());
+        cout << endl
+             << outputcmd << endl;
+        n = write(newsockfd, buff, strlen(buff));
+        if (n < 0)
+            error("ERROR writing to socket");
+    }
+}
+
+void *quit_op(void *param)
+{
+    string s;
+    while(1)
+    {
+        cin>>s;
+        if(s=="quit")
+        {
+            exit(0);
+        }
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    int sockfd, newsockfd, portno;
+    char buffer[256];
+    struct sockaddr_in serv_addr, cli_addr;
+    int n;
+    if (argc < 2)
+    {
+        fprintf(stderr, "ERROR, no port provided\n");
+        exit(1);
+    }
+
+    string line;
+    ifstream readFile(argv[1],ios::in);
+    while (getline(readFile, line));
+    readFile.close();
+    vector<string> v1;
+    stringstream ss(line);
+    while (ss.good())
+    {
+        string sb;
+        getline(ss, sb, ':');
+        v1.push_back(sb);
+    }
+    // IP_SERVER = v1[0];
+    // TRACKER_PORT = stoi(v1[1]);
+    cout << line;
+    portno = stoi(v1[1]);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+        error("ERROR opening socket");
+    bzero((char *)&serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(portno);
+    if (bind(sockfd, (struct sockaddr *)&serv_addr,
+             sizeof(serv_addr)) < 0)
+        error("ERROR on binding");
+    listen(sockfd, 5);
+    socklen_t clilen;
+    clilen = sizeof(cli_addr);
+    int i = 0;
+    pthread_t tid1;
+    if (pthread_create(&tid1, NULL, quit_op, NULL) != 0)
+        printf("Failed to create thread\n");
+    vector<pthread_t> tids;
+    while (1)
+    {
+        pthread_t tid;
+        newsockfd = accept(sockfd,
+                           (struct sockaddr *)&cli_addr,
+                           &clilen);
+        if (pthread_create(&tid, NULL,
+                           reader, (void *)(&newsockfd)) != 0)
+            printf("Failed to create thread\n");
+        // pthread_join(tid[i],NULL);
+        tids.push_back(tid);
+    }
+    close(newsockfd);
+    close(sockfd);
+    return 0;
+}
